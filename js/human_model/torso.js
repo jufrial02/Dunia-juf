b6701cd8ab57eba563b0180bea73f4cdf327a20a
jufrial02 <01jufrial@gmail.com>
2026-06-27 06:12:54 +0800

    Add createTorso function to generate torso model

--- js/human_model/torso.js
+++ js/human_model/torso.js
@@ -1,1 +1,19 @@
+export function createTorso() {
 
+    const group = new THREE.Group();
+
+    const material = new THREE.MeshStandardMaterial({
+        color: 0x444444
+    });
+
+    const body = new THREE.Mesh(
+        new THREE.BoxGeometry(0.45, 0.65, 0.25),
+        material
+    );
+
+    body.position.y = 1.1;
+
+    group.add(body);
+
+    return group;
+}
