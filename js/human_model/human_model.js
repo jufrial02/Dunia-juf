2aa79c5a54a460c98cf0084e307b39384ea49744
jufrial02 <01jufrial@gmail.com>
2026-06-27 06:21:14 +0800

    Import THREE.js and add torso to createHuman

--- js/human_model/human_model.js
+++ js/human_model/human_model.js
@@ -1,7 +1,12 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.128/build/three.module.js";
 import { createTorso } from "./torso.js";
+
 export function createHuman() {
 
     const human = new THREE.Group();
+
+    const torso = createTorso();
+    human.add(torso);
 
     return human;
 
