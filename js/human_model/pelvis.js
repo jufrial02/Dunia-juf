3ba3a11bf5180a59a02767f9c06be19a5d56ec32
jufrial02 <01jufrial@gmail.com>
2026-06-28 03:54:37 +0800

    Update pelvis.js

--- js/human_model/pelvis.js
+++ js/human_model/pelvis.js
@@ -1,17 +1,10 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
 
 export function buatPelvis() {
-    const pelvisGroup = new THREE.Group();
-
-    const geoPelvis = new THREE.BoxGeometry(0.16, 0.06, 0.1);
-    const matPelvis = new THREE.MeshStandardMaterial({ color: 0x333333 }); // Memakai MeshStandard agar merespon cahaya
-    const pelvisMesh = new THREE.Mesh(geoPelvis, matPelvis);
-    
-    pelvisGroup.add(pelvisMesh);
-
-    return {
-        group: pelvisGroup,
-        mesh: pelvisMesh
-    };
+    const group = new THREE.Group();
+    const geo = new THREE.BoxGeometry(0.16, 0.06, 0.1);
+    const mat = new THREE.MeshStandardMaterial({ color: 0x333333 });
+    const mesh = new THREE.Mesh(geo, mat);
+    group.add(mesh);
+    return { group, mesh };
 }
-
